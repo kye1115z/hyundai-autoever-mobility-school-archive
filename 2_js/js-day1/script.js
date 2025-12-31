7429524854ed46f2497aed6085d9903d19612f41
YeEun Kim <kye1115z@naver.com>
2025-12-31 10:20:52 +0900

    js: 출력, 변수, 데이터 타입

---
+++ 2_js/js-day1/script.js
@@ -0,0 +1,19 @@
+// // 1. 출력 (code runner)
+// console.log("Hello world");
+
+// // 2. 변수 선언&초기화&사용
+// const birthYear = 2002;
+// let age = 24;
+// console.log(`birthYear: ${birthYear}, 나이: ${age}`);
+
+// const favFood = "김치볶음밥";
+// let moodScore = 9;
+// const today = "2025-12-31";
+// console.log(`제가 좋아하는 음식은 ${favFood}입니다.`);
+// console.log(`지금 제 기분 점수는 ${moodScore}점 입니다.`);
+// console.log(`오늘 날짜: ${today}.`);
+
+// // 3. 데이터 타입
+const myName = "김예은";
+const age = 24;
+console.log(`안녕하세요. 저는 ${myName}이고, ${age}살입니다.`);
