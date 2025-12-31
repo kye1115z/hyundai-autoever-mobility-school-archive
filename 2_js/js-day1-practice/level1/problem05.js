4077854680ea3fd3087a68e33ec1b31a4a6db737
YeEun Kim <kye1115z@naver.com>
2025-12-31 14:21:19 +0900

    Level 1 - Problem 5 완료

---
+++ 2_js/js-day1-practice/level1/problem05.js
@@ -0,0 +1,9 @@
+const value1 = 42;
+const value2 = "Hello";
+const value3 = true;
+const value4 = undefined;
+
+console.log(typeof value1);
+console.log(typeof value2);
+console.log(typeof value3);
+console.log(typeof value4);
