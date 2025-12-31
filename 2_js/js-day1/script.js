c10aeed247ebe4cce0df467ae03dd1d27b10cafb
YeEun Kim <kye1115z@naver.com>
2025-12-31 11:25:13 +0900

    js: 연산자

--- 2_js/js-day1/script.js
+++ 2_js/js-day1/script.js
@@ -24,5 +24,32 @@
 // console.log(typeof undefined); // undefined
 
 // console.log(Number("a12")); // NaN
-console.log("50" + 30);
-console.log(Number("50") + 30);
+// console.log("50" + 30);
+// console.log(Number("50") + 30);
+
+// // 4. 연산자
+// let count = 1;
+// let count2 = 1;
+// console.log(count++);
+// console.log(++count2);
+
+// let num = 100;
+// num += 50;
+// num /= 2;
+// console.log(num);
+
+// console.log(5 == "5"); // true
+// console.log(5 === "5"); // false
+// console.log(true == 1); // true
+// console.log(true === 1); // false
+
+// const age = 17;
+// const isStudent = false;
+// let isDiscount = "";
+// if (age >= 18 || isStudent) {
+//   isDiscount = "면 할인이 가능합니다.";
+// } else {
+//   isDiscount = " 아니어서 할인이 불가능합니다.";
+// }
+
+// console.log(`18세 이상이거나 학생이${isDiscount}`);
