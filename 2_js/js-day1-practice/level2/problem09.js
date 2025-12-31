a119b00e0679d11f0766dc29b7751f3c1e3ac09f
YeEun Kim <kye1115z@naver.com>
2025-12-31 14:40:53 +0900

    Level 2 - Problem 9 완료

---
+++ 2_js/js-day1-practice/level2/problem09.js
@@ -0,0 +1,26 @@
+const day = 3;
+switch (day) {
+  case 1:
+    console.log("월요일");
+    break;
+  case 2:
+    console.log("화요일");
+    break;
+  case 3:
+    console.log("수요일");
+    break;
+  case 4:
+    console.log("목요일");
+    break;
+  case 5:
+    console.log("금요일");
+    break;
+  case 6:
+    console.log("토요일");
+    break;
+  case 7:
+    console.log("일요일");
+    break;
+  default:
+    console.log("잘못된 입력입니다.");
+}
