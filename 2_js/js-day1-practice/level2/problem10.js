67afb8dd2515a5633a532b5d22862c1ca4480cd0
YeEun Kim <kye1115z@naver.com>
2025-12-31 14:44:02 +0900

    Level 2 - Problem 10 완료

---
+++ 2_js/js-day1-practice/level2/problem10.js
@@ -0,0 +1,14 @@
+const temp = -3;
+if (temp >= 30) {
+  console.log("매우 더움");
+} else if (temp >= 20) {
+  console.log("따뜻함");
+} else if (temp >= 10) {
+  console.log("선선함");
+} else {
+  console.log("추움");
+}
+
+if (temp < 0) {
+  console.log("영하입니다.");
+}
