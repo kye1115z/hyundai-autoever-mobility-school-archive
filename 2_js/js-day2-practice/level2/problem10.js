fe36e088d66f0335b28cbcb77b854176e10413c6
YeEun Kim <kye1115z@naver.com>
2026-01-05 14:43:04 +0900

    Level 2 - Problem 10 완료

---
+++ 2_js/js-day2-practice/level2/problem10.js
@@ -0,0 +1,7 @@
+const getConcat = (list1, list2) => {
+  const combined = list1.concat(list2);
+  return combined;
+};
+const list1 = [1, 2, 3];
+const list2 = [4, 5, 6];
+console.log(getConcat(list1, list2));
