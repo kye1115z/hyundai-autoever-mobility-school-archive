78cce1b96d3bc9dea9a912e5ffeb6527a562fad5
YeEun Kim <kye1115z@naver.com>
2026-01-05 15:14:23 +0900

    Level 3 - Problem 10 완료

---
+++ 2_js/js-day2-practice/level3/problem10.js
@@ -0,0 +1,8 @@
+const studentInfo = [
+  { name: "예은", score: 100 },
+  { name: "여진", score: 101 },
+  { name: "민경", score: 102 },
+];
+for (const student of studentInfo) {
+  console.log(`${student.name}: ${student.score}`);
+}
